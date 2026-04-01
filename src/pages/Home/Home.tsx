ec420f18367beef9fe551d203ec77c193bf16cf2
corsarto <clerc.loic@gmail.com>
2026-04-01 16:40:55 +0200

    change all home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,63 +1,80 @@
 import { Link } from 'react-router-dom'
 import './home.scss'
-import { useQuery } from '@tanstack/react-query'
-import type  { QuizType }  from '../../types';
+import { useState } from 'react';
 
 export default function Home() {
-    const fetchQuiz = async () => {
-    const res = await fetch('https://quizzapi.jomoreschi.fr/api/v2/quiz');
-    if (!res.ok) throw new Error('Erreur API');
-    return res.json();
-  };
-   const { data } = useQuery({
-    queryKey: ['quiz'],
-    queryFn: fetchQuiz
-});
+    
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const categories = [
+        {label: 'Toutes catégories', value: 'all'},
+        {label: 'Sport', value: 'sport'},
+        {label: 'Histoire', value: 'histoire'},
+        {label: 'Géographie', value: 'geographie'},
+        {label: 'Science', value: 'science'}, 
+        {label: 'Musique', value: 'musique'},
+        {label: 'TV et cinéma', value: 'tv_cinema'},
+        {label: 'Culture générale', value: 'culture_generale'},
+        {label: 'Art et littérature', value: 'art_litterature'},
+        {label: 'Gastronomie', value: 'gastronomie'},
+        {label: 'Actu politique', value: 'actu_politique'}, 
+        {label: 'Jeux vidéos', value: 'jeux_videos'},
+    ];
 
-    const allCategories: string[] = data
-    ? Array.from(new Set (data.quizzes.map((q: QuizType) => q.category)))
-    : [];
-
-    const allDifficulties: string[] = data
-    ? Array.from(new Set (data.quizzes.map((q: QuizType) => q.difficulty)))
-    : [];
+    const [selectedDifficulty, setSelectedDifficulty] = useState('');
+    const difficulties = [
+        {label: 'Toutes difficultés', value: 'all'},
+        {label: 'Facile', value: 'facile'},
+        {label: 'Moyen', value: 'normal'},
+        {label: 'Difficile', value: 'difficile'},
+    ];
+    
+    const [selectedQuestions, setSelectedQuestions] = useState('');
 
     return (
         <div>
             <h2>Bienvenue sur Quiz Culture !</h2>
             <div className='quiz-container'>
-                <p>Veuillez séléctionner un quiz :</p>
-                <div className='card-quiz'>
-                    <Link to="/quiz/5">
-                        <div className='quiz-option'>5 questions</div>
-                    </Link>
-                    <Link to="/quiz/10">
-                        <div className='quiz-option'>10 questions</div>
-                    </Link>
-                    <Link to="/quiz/15">
-                        <div className='quiz-option'>15 questions</div>
-                    </Link>
+                <h3>Veuillez séléctionner un quiz </h3>
+                <div className='questions-container'>
+                    <p>Séléctionner le nombre de questions :</p>
+                    <select 
+                    value={selectedQuestions} 
+                    onChange={(e) => setSelectedQuestions(e.target.value)}>
+                        <option value="">--Choisir le nombre de questions--</option>
+                        <option value="5">5 questions</option>
+                        <option value="10">10 questions</option>
+                        <option value="15">15 questions</option>
+                    </select>
                 </div>
                 <div className='categories-container'>
                     <p>Séléctionner une catégorie :</p>
-                    <div className='categories-list'>
-                        <ul className='category-option'>
-                            {allCategories.map((category, i) => (
-                                <li key={i}>{category}</li>
-                            ))}
-                        </ul>
-                    </div>
+                    <select 
+                    value={selectedCategory} 
+                    onChange={(e) => setSelectedCategory(e.target.value)}>
+                        <option value="">--Choisir une catégorie--</option>
+                        {categories.map((category, i) => (
+                            <option key={i} value={category.value}>
+                                {category.label}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div className='difficulties-container'>
                     <p>Séléctionner une difficulté : </p>
-                    <div className='difficulties'>
-                        <ul className='difficulty-option'>
-                            {allDifficulties.map((difficulty, i) => (
-                                <li key={i}>{difficulty}</li>
-                            ))}
-                        </ul>
-                    </div>
+                    <select 
+                    value={selectedDifficulty} 
+                    onChange={(e) => setSelectedDifficulty(e.target.value)}>
+                        <option value="">--Choisir une difficulté--</option>
+                        {difficulties.map((difficulty, i) => (
+                            <option key={i} value={difficulty.value}>
+                                {difficulty.label}
+                            </option>
+                        ))}
+                    </select>
                 </div>
+                <Link to={`/quiz/${selectedQuestions}?category=${selectedCategory}&difficulty=${selectedDifficulty}`}>
+                    <button>Lancer le quiz</button>
+                </Link>
             </div>
         </div>    
     )
